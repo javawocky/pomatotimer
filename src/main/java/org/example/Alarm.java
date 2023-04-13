28061f7e9707cdb955e47442dabf185b4a0d51ee
Elton Fry <elton.fry@asknice.ly>
2023-04-14 10:23:45 +1200

    First time commit

---
+++ src/main/java/org/example/Alarm.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.sound.sampled.*;
+
+public class Alarm {
+
+    public static void playAlarm() throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+        String filename = "owl.wav";
+        InputStream inputStream = new BufferedInputStream(Main.class.getResourceAsStream("/" + filename));
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+        // Thread.sleep(clip.getMicrosecondLength() / 1000);
+    }
+
+    public static void playBegin() throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+        String filename = "begin.wav";
+        InputStream inputStream = new BufferedInputStream(Main.class.getResourceAsStream("/" + filename));
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+        // Thread.sleep(clip.getMicrosecondLength() / 1000);
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, UnsupportedAudioFileException, LineUnavailableException, IOException {
+        int workMinutes = getUserInput("work", 20);
+        int breakMinutes = getUserInput("break", 5);
+        while(true) {
+            System.out.println("\nStarting timer...");
+
+            Alarm.playBegin();
+
+            int totalSeconds = workMinutes * 60;
+            for (int i = totalSeconds; i > 0; i--) {
+                String progress = getProgressBar(i, totalSeconds);
+                System.out.print("\r" + progress + " " + getTimeRemaining(i));
+                Thread.sleep(1000);
+            }
+
+            System.out.println("\nTime for a break!");
+            Alarm.playAlarm();
+            totalSeconds = breakMinutes * 60;
+            for (int i = totalSeconds; i > 0; i--) {
+                String progress = getProgressBar(i, totalSeconds);
+                System.out.print("\r" + progress + " " + getTimeRemaining(i));
+                Thread.sleep(1000);
+            }
+
+            System.out.println("\nTimer finished!");
+        }
+    }
+
+    private static int getUserInput(String type, int defaultValue) {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        do {
+            System.out.print("Enter a number between 1 and 59 (" + type + " minutes, default " + defaultValue + "): ");
+            String userInput = scanner.nextLine();
+            if (userInput.isEmpty()) {
+                input = defaultValue;
+                break;
+            }
+            while (!userInput.matches("\\d+") || (input = Integer.parseInt(userInput)) < 1 || input > 59) {
+                System.out.println("That's not a valid number!");
+                System.out.print("Enter a number between 1 and 59 (" + type + " minutes, default " + defaultValue + "): ");
+                userInput = scanner.nextLine();
+                if (userInput.isEmpty()) {
+                    input = defaultValue;
+                    break;
+                }
+            }
+        } while (input < 1 || input > 59);
+        return input;
+    }
+
+    private static String getProgressBar(int remainingSeconds, int totalSeconds) {
+        int numFilled = (totalSeconds - remainingSeconds) * 7 / totalSeconds;
+        int numEmpty = 7 - numFilled;
+        String[] spinners = {"|", "\\", "-", "/"};
+        int spinnerIndex = remainingSeconds % spinners.length;
+        return "[" + "#".repeat(numFilled) + spinners[spinnerIndex] + "-".repeat(numEmpty) + "]" ;
+    }
+
+    private static String getTimeRemaining(int remainingSeconds) {
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+        return String.format("%02d:%02d remaining", minutes, seconds);
+    }
+}
